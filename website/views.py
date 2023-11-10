8b9e28898f9fe81ee3dae06ea34838f2f57085ad
HyperTTPS <jonas10bork@gmail.com>
2023-11-10 13:44:54 +0100

    aoeu

--- website/views.py
+++ website/views.py
@@ -7,13 +7,13 @@ from .models import *
 # Create your views here.
 def index(request):
     context = {"shifts_and_shows": [
-        Shift(1, datetime.now(), datetime.now(), [], GroupType.PR, False),
-        Shift(2, datetime.now(), datetime.now(), [], GroupType.CLEANING, False),
-        Shift(3, datetime.now(), datetime.now(), [], GroupType.SALES, False),
+        Shift(1, datetime.now(), datetime.now(), [], GroupType.PR, False, 2),
+        Shift(2, datetime.now(), datetime.now(), [], GroupType.CLEANING, False, 1),
+        Shift(3, datetime.now(), datetime.now(), [], GroupType.SALES, False, 3),
         Show("Barbie", ShowType.EVENT, datetime.now(), datetime.now(), 1),
-        Shift(4, datetime.now(), datetime.now(), [], GroupType.TECHNICAL, False),
+        Shift(4, datetime.now(), datetime.now(), [], GroupType.TECHNICAL, False, 2),
         Show("Avengers", ShowType.EVENT, datetime.now(), datetime.now(), 2),
-        Shift(5, datetime.now(), datetime.now(), [], GroupType.FACILITY_SERVICE, False),
+        Shift(5, datetime.now(), datetime.now(), [], GroupType.FACILITY_SERVICE, False, 1),
         Show("The Adventures of Slaub", ShowType.EVENT, datetime.now(), datetime.now(), 3),
     ]}
     return HttpResponse(render(request, "website/schedule.html", context))
