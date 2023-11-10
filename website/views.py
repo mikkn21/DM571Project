e54d812ca09e7462acb78939494bbd545b4064c1
HyperTTPS <jonas10bork@gmail.com>
2023-11-10 12:10:29 +0100

    Added test data for the schedule

--- website/views.py
+++ website/views.py
@@ -6,7 +6,13 @@ from .models import *
 
 # Create your views here.
 def index(request):
-    context = {"arr": [1,2,3,4], "shift": Shift(1, datetime.now(), datetime.now(), [], enum_group.Group.PR, False)}
+    context = {"shift_and_shows": [
+        Shift(1, datetime.now(), datetime.now(), [], enum_group.Group.PR, False),
+        Shift(2, datetime.now(), datetime.now(), [], enum_group.Group.CLEANING, False),
+        Shift(3, datetime.now(), datetime.now(), [], enum_group.Group.SALES, False),
+        Shift(4, datetime.now(), datetime.now(), [], enum_group.Group.TECHNICAL, False),
+        Shift(5, datetime.now(), datetime.now(), [], enum_group.Group.FACILITY_SERVICE, False),
+    ]}
     return HttpResponse(render(request, "website/schedule.html", context))
 
 def test(request):
