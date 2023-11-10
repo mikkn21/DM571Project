1c43aa0c63feecfa92395863d40f5ed1e2b4d3f5
sofus <sofus@laubel.net>
2023-11-10 12:16:05 +0100

    database delete and insert

--- website/models/database.py
+++ website/models/database.py
@@ -1,12 +1,17 @@
-from typing import List, TypedDict
+from typing import List, Dict, TypedDict
 from member import Member
 from show import Show
 from shift import Shift
+
 
 class DatabaseRecords(TypedDict):
     members: List[Member]
     shifts: List[Shift]
     shows: List[Show]
+
+
+
+
 
 class MockDatabase:
     def __init__(self):
@@ -16,20 +21,12 @@ class MockDatabase:
             'shows': []
         }
 
-    def insert(self, table, record):
+    def insert(self, table: str, record):
         self._data[table].append(record)
 
-    def query(self, table, conditions):
-        # Implement a simple query logic
-        return [record for record in self._data[table] if all(record[k] == v for k, v in conditions.items())]
+    def delete(self, table: str, condition: Dict[str, any]):
+        [self._data[table].remove(record) for record in self._data[table] if all(record.__dict__[k] == v for k,v in condition.items())]
 
-    def update(self, table, conditions, update_values):
-        # Implement update logic
-        for record in self._data[table]:
-            if all(record[k] == v for k, v in conditions.items()):
-                for k, v in update_values.items():
-                    record[k] = v
+    def update(self, table: str, condition: Dict[str, str], updated_values: Dict[str, str]):
+        [n for n, record in self._data[table]]
 
-    def delete(self, table, conditions):
-        # Implement delete logic
-        self._data[table] = [record for record in self._data[table] if not all(record[k] == v for k, v in conditions.items())]
