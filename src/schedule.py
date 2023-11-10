c86129c0404b89c788094466785e999a30404e2c
sofus <sofus@laubel.net>
2023-11-10 10:59:30 +0100

    made classes member, schedule, shfit and show+enum

---
+++ src/schedule.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from typing import List
+from shift import Shift
+from show import Show
+
+class Schedule:
+    def __init__(self, _from: datetime, _to: datetime, shows: List[Show], shifts: List[Shift]):
+        self._from: datetime = _from
+        self._to: datetime = _to
+        self.shows: List[Show] = shows
+        self.shifts: List[Shift] = shifts
---
+++ src/show.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from enum_show_type import Show
+
+class Show:
+    def __init__(self, title: str, type: Show, _from: datetime, _to: datetime, id: int):
+        self.title: str = title
+        self.type: Show = type
+        self._from: datetime = _from
+        self._to: datetime = _to
+        self.id: int = id
