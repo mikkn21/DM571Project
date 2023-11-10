f0b720b80da73cf56289757c430a73a0597d2425
HyperTTPS <jonas10bork@gmail.com>
2023-11-10 13:09:16 +0100

    aoeu

--- website/models/__init__.py
+++ website/models/__init__.py
@@ -1,6 +1,6 @@
 from .shift import Shift
 from .show import Show
 from .enum_show_type import ShowType
-from .enum_group import Group
+from .enum_group import GroupType
 from .member import Member
 from .schedule import Schedule
