d8bc35a7716f8b73dcf6dc6a34444c503dbe3478
sofus <sofus@laubel.net>
2023-11-14 11:12:54 +0100

    unit tests examples

---
+++ website/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from ..models import Database, Condition
+from ..models.member import Member
+from ..models.shift import Shift
+
+class DatabaseTestCase(TestCase):
+    def setUp(self):
+        self.db = Database()
+        self.db.insert('members', Member('Alice'))
+        self.db.insert('members', Member('Bob'))
+
+    def test_insert(self):
+        self.assertEqual(len(self.db._data['members']), 2)
+
+    def test_delete(self):
+        condition = Condition(key='name', value='Alice')
+        self.db.delete('members', [condition])
+        self.assertEqual(len(self.db._data['members']), 1)
+
+    # Add more tests for update, get, and other functionalities
+
+class ConditionTestCase(TestCase):
+    def test_initialization(self):
+        condition = Condition(key='name', value='Alice')
+        self.assertEqual(condition.key, 'name')
+        self.assertEqual(condition.value, 'Alice')
+
+    def test_custom_comparison_function(self):
+        condition = Condition(key='name', value='Alice', compare=lambda x, y: x != y)
+        self.assertFalse(condition.compare('Alice', 'Alice'))
+
+    # Add more tests as needed
