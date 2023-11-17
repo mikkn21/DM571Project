bfafa8cea2f3f0d1b40f74280a79d5dc3ee399a1
Jonas <jonas10bork@gmail.com>
2023-11-17 09:55:31 +0100

    Finished the layout of the schedule.html

--- website/views.py
+++ website/views.py
@@ -11,18 +11,24 @@ def index(request):
         Shift(2, datetime.now(), datetime.now(), [], GroupType.CLEANING, False, 1),
         Shift(3, datetime.now(), datetime.now(), [], GroupType.SALES, False, 3),
         Show("Barbie", ShowType.EVENT, datetime.now(), datetime.now(), 1),
-        Shift(4, datetime.now(), datetime.now(), [], GroupType.TECHNICAL, False, 2),
+        Shift(4, datetime(2023, 8, 13, 6, 14, 47), datetime(2023, 8, 13, 10, 17, 47), [], GroupType.TECHNICAL, False, 2),
         Show("Avengers", ShowType.EVENT, datetime.now(), datetime.now(), 2),
         Shift(5, datetime.now(), datetime.now(), [], GroupType.FACILITY_SERVICE, False, 3),
         Show("The Adventures of Slaub", ShowType.EVENT, datetime.now(), datetime.now(), 3),
     ]
 
+    shifts_and_shows.sort(key=lambda shift_or_show: shift_or_show.start_date)
+
+    last_shift_or_show = None;
     for shift_or_show in shifts_and_shows:
+        if last_shift_or_show == None or last_shift_or_show.start_date.date() != shift_or_show.start_date.date():
+            shift_or_show.new_date = shift_or_show.start_date.date().strftime("%d/%m/%Y")
         shift_or_show.start_date_hour_min = shift_or_show.start_date.strftime("%H:%M")
         shift_or_show.end_date_hour_min = shift_or_show.end_date.strftime("%H:%M")
         if isinstance(shift_or_show, Shift):
             shift_or_show.booked_members_count = len(shift_or_show.booked_members)
             shift_or_show.is_bookable = True
+        last_shift_or_show = shift_or_show
 
     shifts_and_shows = [(isinstance(shift_or_show, Shift), shift_or_show) for shift_or_show in shifts_and_shows] # map list to (is_shift, shift_or_show)
     context = {"shifts_and_shows": shifts_and_shows}
